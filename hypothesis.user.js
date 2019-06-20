027c2955e4df01745ad65e095fbfd3cbe01f93bf
Diego de la Hera <delahera@gmail.com>
2019-06-20 19:33:27 -0300

    Create hypothesis.user.js

---
+++ hypothesis.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name            Hypothesis Web Annotation
+// @grant    				none
+// ==/UserScript==
+
+(
+	function(){
+		window.hypothesisConfig=function(){
+			return{showHighlights:true,appType:'bookmarklet'};
+		};
+		var d=document,s=d.createElement('script');
+		s.setAttribute('src','https://hypothes.is/embed.js');
+		d.body.appendChild(s)
+	}
+)();
